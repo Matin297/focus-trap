e2d8e248b366e9554ac02574cb04ef306889ff7a
Matin Mir <asmamir297@gmail.com>
2024-05-11 16:11:46 +0330

    implement get node tab index utility function

---
+++ components/FocusTrap/utils.ts
@@ -0,0 +1,27 @@
+function getNodeTabIndex(node: HTMLElement) {
+  const tabIndexAttr = node.getAttribute("tabindex");
+  const parsedTabIndexAttr = parseInt(tabIndexAttr || "");
+
+  if (!Number.isNaN(parsedTabIndexAttr)) {
+    return parsedTabIndexAttr;
+  }
+
+  // ** Special case **
+  // Browsers do not return `tabIndex` correctly for contentEditable nodes;
+  // so if they don't have a tabindex attribute specifically set, assume it's 0.
+  //
+  // in Chrome, <details/>, <audio controls/> and <video controls/> elements get a default
+  // `tabIndex` of -1 when the 'tabindex' attribute isn't specified in the DOM,
+  // yet they are still part of the regular tab order;
+  if (
+    (node.isContentEditable ||
+      node.tagName === "AUDIO" ||
+      node.tagName === "VIDEO" ||
+      node.tagName === "DETAILS") &&
+    tabIndexAttr === null
+  ) {
+    return 0;
+  }
+
+  return node.tabIndex;
+}
