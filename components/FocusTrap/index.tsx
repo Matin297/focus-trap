70ed381f2bd104d4af1013e790d9371ac6ee0bd3
Matin Mir <asmamir297@gmail.com>
2024-05-11 19:42:47 +0330

    add focus trap skeleton and basic props

---
+++ components/FocusTrap/index.tsx
@@ -0,0 +1,5 @@
+import { FocusTrapProps } from "./types";
+
+export default function FocusTrap({ children }: FocusTrapProps) {
+  return <div>{children}</div>;
+}
--- components/FocusTrap/types.ts
+++ components/FocusTrap/types.ts
@@ -1,5 +1,11 @@
+import { ReactElement } from "react";
+
 export interface OrderedTabbableNode {
   node: HTMLElement;
   tabIndex: number;
   documentIndex: number;
+}
+
+export interface FocusTrapProps {
+  children: ReactElement;
 }
